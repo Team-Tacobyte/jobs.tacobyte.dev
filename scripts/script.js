8e34aa865996cd31cf5ffdc7930502e12655a69f
tibthink <bent23jam@gmail.com>
2022-03-27 21:13:21 -0400

    setting up pages for diffrent jobs

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
 function redirect() {
     window.location.href = "https://forms.gle/BC36HuCQDAo1scUc7";
 }
+
+function community_redirect() {
+    window.location.href = "/pages/jobs/community/index.html";
+}
+
+function customer_redirect() {
+    window.location.href = "/pages/jobs/customer/index.html";
+}
+
+function design_redirect() {
+    window.location.href = "/pages/jobs/design/index.html";
+}
+
+function engineering_redirect() {
+    window.location.href = "/pages/jobs/engineering/index.html";
+}
+
+function moderation_redirect() {
+    window.location.href = "/pages/jobs/moderation/index.html";
+}
+function product_redirect() {
+    window.location.href = "/pages/jobs/product/index.html";
+}
